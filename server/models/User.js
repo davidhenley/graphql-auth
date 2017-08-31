53a7c872393a5f72a26143a8e3802d5e5d0fb38f
David Henley <henleydr@gmail.com>
2017-08-31 09:37:28 -0500

    User model methods

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,32 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const bcrypt = require('bcrypt');
+
 const UserSchema = new Schema({
   username: String,
   password: String
 });
 
-module.exports = mongoose.model('User');
+UserSchema.pre('save', function(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+
+
+
+module.exports = mongoose.model('User', UserSchema);
