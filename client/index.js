da21ba896b2df8d873b8883c636b9e8fed6eca13
David Henley <henleydr@gmail.com>
2017-08-30 23:25:57 -0500

    React boilerplate

--- client/index.js
+++ client/index.js
@@ -1,1 +1,12 @@
-console.log('Hello!');
+import React from 'react';
+import { render } from 'react-dom';
+
+const Root = () => {
+  return (
+    <div>
+      Hello There
+    </div>
+  );
+}
+
+render(<Root />, document.querySelector('#root'));
