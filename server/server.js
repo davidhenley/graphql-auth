f895141f58afc9af3bf6ef3b2bfcc09114543531
David Henley <henleydr@gmail.com>
2017-08-30 22:56:22 -0500

    Added mongoose

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const keys = require('./config/keys');
+
+mongoose.connect(keys.MONGO_URI);
+
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance'))
+  .on('error', (e) => console.log('Error connecting to MongoLab', e));
+
 app.get('/', (req, res) => {
   res.send('Hi there');
 });
